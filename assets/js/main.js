9f14c71b2cd6517e660d8deeab7b159bf32eaf06
martinapaiz <martupaiz@gmail.com>
2025-07-17 12:36:17 -0300

    Entrega Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,7 +162,7 @@
       if (!navmenulink.hash) return;
       let section = document.querySelector(navmenulink.hash);
       if (!section) return;
-      let position = window.scrollY + 200;
+      let position = window.scrollY + offset + 10;
       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
         document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
         navmenulink.classList.add('active');
